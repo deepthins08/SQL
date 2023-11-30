662a9a3af50dd60cea27a398caf0f01d1d519d63
deepthins08 <149232424+deepthins08@users.noreply.github.com>
2023-11-30 20:19:21 +0530

    nov 30 task

---
+++ nov30.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Olympic_Games(id int,name varchar(20),sport varchar(15),year bigint,country varchar(20),
+  gender varchar(20),age int,city varchar(20),country_code varchar(20),medal varchar(10));
+INSERT INTO Olympic_Games values("1","chetan","running","2013","india","male","23","tokyo","ind","gold");
+INSERT INTO Olympic_Games values("2","manu","shotput","2014","china","male","26","tokyo","chi","bronze");
+INSERT INTO Olympic_Games values("3","prajwal","swimming","2016","srilanka","male","24","London","sl","silver");
+INSERT INTO Olympic_Games values("4","Harish","javalin","2015","Bangladesh","male","25","Paris","ban","bronze");
+INSERT INTO Olympic_Games values("5","Sumit","Badminton","2016","England","male","26","Paris","Eng","bronze");
+INSERT INTO Olympic_Games values("6","Naveen","Boxing","2017","pakistan","male","27","Tokyo","pak","Gold");
+INSERT INTO Olympic_Games values("7","Deepthi","Diving","2016","Japan","Female","26","Tokyo","Jap","Gold");
+INSERT INTO Olympic_Games values("8","Brunda","Cycling","2015"," Bermuda","Female","25","Tokyo","Ber","Bronze");
+INSERT INTO Olympic_Games values("9","Archana","Shooting","2016"," Bhutan","Female","26","Tokyo","Bhu","Gold");
+INSERT INTO Olympic_Games values("10","Raju","Karate","2017"," Brazil","Male","27","Paris","Bra","Silver");
+INSERT INTO Olympic_Games values("11","Kiran","Rugby","2016"," Bulgaria","Male","26","Paris","Bul","Gold");
+INSERT INTO Olympic_Games values("12","Madhu","Snowboarding","2017","  Canada","Male","26","Landon","Can","Silver");
+INSERT INTO Olympic_Games values("13","Shivu","Tug of War","2018","  Colombia","Male","28","Landon","Col","Gold"); 
+INSERT INTO Olympic_Games values("14","Sahana","Fencing","2017","Cuba","Female","23","Tokyo","Cub","Gold"); 
+INSERT INTO Olympic_Games values("15","Puja","Judo","2016","Denmark","Female","28","Tokyo","Den","Silver"); 
+INSERT INTO Olympic_Games values("16","Sneha","Archery","2017","France","Female","25","Tokyo","Fra","Bronze"); 
+INSERT INTO Olympic_Games values("17","Pavan","Biathlon","2018","Germany","Male","27","Tokyo","Ger","Gold"); 
+INSERT INTO Olympic_Games values("18","Satya","Weightlifting","2017","Greece","Male","26","Landon","Gre","Silver"); 
+INSERT INTO Olympic_Games values("19","Prakash","Water polo","2016","Grenada","Male","26","Landon","Grn","Gold"); 
+INSERT INTO Olympic_Games values("20","keerthi","Shooting","2018","india","Female","23","Landon","ind","Gold"); 
+
